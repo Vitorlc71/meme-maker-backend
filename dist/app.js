a0ecf70e4b6274e900cd1a1e0891170dc45df46f
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-02-26 12:30:45 -0300

    rename app

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,7 @@ var express_1 = __importDefault(require("express"));
 var routes_1 = __importDefault(require("./routes"));
 var cors_1 = __importDefault(require("cors"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var PORT = process.env.PORT || 3333;
 var app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use(function (req, res, next) {
@@ -19,4 +20,4 @@ app.use(body_parser_1.default.urlencoded({
     extended: true
 }));
 app.use(routes_1.default);
-app.listen(3333);
+app.listen(PORT);
