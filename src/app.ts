4a7dcb928e762f40d937cbe685a767b6e34467bf
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-02-26 14:16:13 -0300

    headers config

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+    res.header('Access-Control-Allow-Headers: Origin, Content-Type, application/json')
     app.use(cors())
     next()
 })
