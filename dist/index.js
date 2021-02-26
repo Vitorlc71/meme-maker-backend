beb3d1c22934ba768fe6f03d2911083825e91e94
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-02-26 11:01:02 -0300

    js compiler

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = __importDefault(require("./routes"));
+var cors_1 = __importDefault(require("cors"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
-app.get('/', function (req, res) {
-    return res.send('Hello World!!!');
+app.use(body_parser_1.default.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    app.use(cors_1.default());
+    next();
 });
+app.use(body_parser_1.default.urlencoded({
+    extended: true
+}));
+app.use(routes_1.default);
 app.listen(3333);
