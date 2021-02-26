2790fe888eff1d346d879b6fd46a56388f62f2be
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-02-26 09:35:56 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
     app.use(cors())
     next()
