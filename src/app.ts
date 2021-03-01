bf6b6afa2ed50b26ea0beb62dd6c23ce63c556e5
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-03-01 10:27:54 -0300

    cors origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "https://maker-meme.herokuapp.com/")
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
     res.header("Access-Control-Allow-Headers","*")
     app.use(cors())
