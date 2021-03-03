e69fcd2cd1a7dfb9a47ea1ba0fc9ba353d51e37f
Vitorlc71 <vitor_lucascarvalho@hotmail.com>
2021-03-03 18:51:04 -0300

    cors again

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,6 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://maker-meme.herokuapp.com/")
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
     res.header("Access-Control-Allow-Headers","*")
     app.use(cors())
